dd18731307d84af9c0a728167f894a8de12750a3
arnaud ferrand <arferrand@gmail.com>
2016-11-09 11:19:04 +0000

    updates to api v2, adds a sample html page

--- 3words-button.js
+++ 3words-button.js
@@ -1,15 +1,47 @@
-$('button').on('click', function (e) {
+$('button').on('click', function(e) {
     var t = $('span');
-    t.text('Loading...');
-    
-    navigator.geolocation.getCurrentPosition(function (location) {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(show3WordAddress, showError);
+        t.text('Loading...');
+    } else {
+        t.text('Geolocation is not supported by this browser.');
+    }
+
+    function show3WordAddress(location) {
         data = {
-            'key':		'insertAPIkey',
-            'position':	location.coords.latitude + ',' + location.coords.longitude
+            key: 'YOUR-W3W-API-KEY',
+            coords: location.coords.latitude + ',' + location.coords.longitude
         };
-        
-        $.post('http://api.what3words.com/position', data, function(response) {
-            t.text(response.words.join('.'));
+
+        $.ajax({
+            url: 'https://api.what3words.com/v2/reverse',
+            type: 'get',
+            data: data,
+            success: function(response) {
+                t.text(response.words);
+            },
+            error: function(xhr, status, error) {
+                var err = JSON.parse(xhr.responseText);
+                t.text(err.message);
+            }
         });
-    });  
+    }
+
+    function showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                t.text('User denied the request for Geolocation.');
+                break;
+            case error.POSITION_UNAVAILABLE:
+                t.text('Location information is unavailable.');
+                break;
+            case error.TIMEOUT:
+                t.text('The request to get user location timed out.');
+                break;
+            case error.UNKNOWN_ERROR:
+                t.text('An unknown error occurred.');
+                break;
+        }
+    }
 });
--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-3words-button
-=============
+# <img src="https://what3words.com/assets/w3w_square_red.png" width="64" height="64" alt="what3words">&nbsp;3words-button
 
 JavaScript for button to show the 3 word address for the user's current location
 
-Get your API key at http://what3words.com/api/signup
+## Get started
+Get your API key at https://map.what3words.com/register?dev=true
+
+Set your API in `3words-button.js`
+
+```javascript
+function show3WordAddress(location) {
+    data = {
+        key: 'YOUR-W3W-API-KEY',
+        coords: location.coords.latitude + ',' + location.coords.longitude
+    };
+```
+
+Then run the sample html file.
+
+Warning:  Modern browsers permits finding current location only over HTTPS.
+
+## Licensing
+
+MIT
